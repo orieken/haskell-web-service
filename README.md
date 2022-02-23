de65f0595078ac3842b7a869c8d88878c6f25a4e
Rieken <oscar.rieken@adp.com>
2022-02-22 22:20:46 -0500

    :green_heart: (heroku) setup to deploy

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # haskell-web-service
 just a simple service using Scotty 
 
+### Status
+![example workflow](https://github.com//orieken/haskell-web-service/actions/workflows/haskell.yml/badge.svg)
--- src/Main.hs
+++ src/Main.hs
@@ -3,8 +3,10 @@ module Main where
 
 import qualified App
 import Web.Scotty
+import System.Environment.MrEnv ( envAsInt )
 
 main :: IO ()
 main = do
+  port <- envAsInt "PORT" 4400
   putStrLn "Starting Server...."
-  scotty 4000 App.app
+  scotty port App.app
